2bf6b6f058dae5f092fb8c5de8f39419ca1e1002
kebricsysair <klara.kebric@systemair.si>
2023-03-17 15:11:35 +0100

    ADDED Oddelek

---
+++ imenik/src/main/java/si/systemair/imenik/models/Oddelek.java
@@ -0,0 +1,5 @@
+package si.systemair.imenik.models;
+
+public enum Oddelek {
+    ODDELEK1, ODDELEK2, ODDELEK3
+}
--- imenik/src/main/java/si/systemair/imenik/models/User.java
+++ imenik/src/main/java/si/systemair/imenik/models/User.java
@@ -32,19 +32,22 @@ public class User {
     private Integer number;
     @Getter @Setter
     private Integer postcode;
+    @Getter @Setter
+    private Oddelek department;
 
     public User() {
 
     }
-    public User(String name, String surname,String company, String email){
+    public User(String name, String surname, String company, String email, Oddelek department){
         this();
         this.name = name;
         this.surname = surname;
         this.company = company;
         this.email = email;
+        this.department = department;
     }
-    public User(String name, String surname,String company, String email, String jobTitle, String businessPhone, String mobilePhone, Integer number, Integer postcode){
-        this(name, surname,company, email);
+    public User(String name, String surname, String company, String email, String jobTitle, String businessPhone, String mobilePhone, Integer number, Integer postcode, Oddelek department){
+        this(name, surname,company, email, department);
         this.jobTitle = jobTitle;
         this.businessPhone = businessPhone;
         this.mobilePhone = mobilePhone;
